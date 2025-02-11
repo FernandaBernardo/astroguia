0b5693006813471217db6c36cdf6c9d1dbafda77
Fernanda Bernardo <fernandabernardo94@gmail.com>
2025-02-11 17:15:59 -0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -270,7 +270,7 @@ function App() {
               <div className="mb-8">
                 <div className="text-2xl line-through text-gray-400 mb-2">De R$297,00</div>
                 <div className="text-4xl md:text-6xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-yellow-300 via-yellow-200 to-yellow-300 mb-4">
-                  12x de R$14,99
+                  <span className="text-2xl text-transparent bg-clip-text bg-gradient-to-r from-yellow-300 via-yellow-200 to-yellow-300 mb-2">12 x</span> R$14,99
                 </div>
                 <div className="text-xl mb-2">ou R$149,90 à vista!</div>
                 <p className="text-gray-300 text-sm">Economia de R$147,10</p>
